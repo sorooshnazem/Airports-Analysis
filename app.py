1ed5ed027c3eecfd60e9d96476de25b46e96cda9
Soroosh Nazem <soroosh.nazem@gmail.com>
2026-06-08 15:47:16 +0200

    Add runway analysis using merge

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 st.title("Airport Business Intelligence Dashboard")
 
 airports = pd.read_csv("data/airports.csv")
+runways = pd.read_csv("data/runways.csv")
 
 countries = sorted(
     airports["iso_country"]
@@ -161,4 +162,72 @@ st.dataframe(
             "is_above_type_average"
         ]
     ].head(100)
+)
+
+st.subheader("Runway Analysis")
+
+st.write("Preview of runways dataset")
+st.dataframe(runways.head())
+
+airport_runways = airports.merge(
+    runways,
+    left_on="id",
+    right_on="airport_ref",
+    how="inner"
+)
+
+st.write("Airports joined with runways")
+st.dataframe(
+    airport_runways[
+        [
+            "ident",
+            "name",
+            "type",
+            "iso_country",
+            "airport_ident",
+            "length_ft",
+            "width_ft",
+            "surface",
+            "lighted",
+            "closed"
+        ]
+    ].head()
+)
+
+runway_stats = (
+    airport_runways
+    .groupby(["ident", "name"], as_index=False)
+    .agg(
+        number_of_runways=("airport_ref", "size"),
+        max_runway_length=("length_ft", "max"),
+        average_runway_length=("length_ft", "mean"),
+        average_runway_width=("width_ft", "mean"),
+        number_of_lighted_runways=("lighted", "sum"),
+        number_of_closed_runways=("closed", "sum")
+    )
+    .sort_values("number_of_runways", ascending=False)
+)
+
+st.subheader("Top Airports by Number of Runways")
+
+top_runway_airports = runway_stats.head(20)
+
+st.dataframe(top_runway_airports)
+
+st.bar_chart(
+    top_runway_airports.set_index("ident")["number_of_runways"]
+)
+
+st.subheader("Top Airports by Maximum Runway Length")
+
+top_length_airports = (
+    runway_stats
+    .sort_values("max_runway_length", ascending=False)
+    .head(20)
+)
+
+st.dataframe(top_length_airports)
+
+st.bar_chart(
+    top_length_airports.set_index("ident")["max_runway_length"]
 )
