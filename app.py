6fba1719ebdad92cd7610bf4396c89c6a2ee7407
Soroosh Nazem <soroosh.nazem@gmail.com>
2026-06-08 02:34:01 +0200

    Add top countries airport analysis

--- app.py
+++ app.py
@@ -80,3 +80,20 @@ st.dataframe(country_types)
 st.bar_chart(
     country_types.set_index("type")["number_of_airports"]
 )
+
+st.subheader("Top Countries by Number of Airports")
+
+top_countries = (
+    airports
+    .groupby("iso_country", as_index=False)
+    .size()
+    .rename(columns={"size": "number_of_airports"})
+    .sort_values("number_of_airports", ascending=False)
+    .head(10)
+)
+
+st.dataframe(top_countries)
+
+st.bar_chart(
+    top_countries.set_index("iso_country")["number_of_airports"]
+)
