8d6c01d8b9827ded4dbe66f930351271b3180b42
Soroosh Nazem <soroosh.nazem@gmail.com>
2026-06-08 15:30:39 +0200

    Add airport elevation transform analysis

--- app.py
+++ app.py
@@ -119,3 +119,46 @@ st.dataframe(type_stats)
 st.bar_chart(
     type_stats.set_index("type")["average_elevation"]
 )
+
+st.subheader("Airport Elevation Compared to Type Average")
+
+airports_transform = airports.copy()
+
+airports_transform["average_elevation_by_type"] = (
+    airports_transform
+    .groupby("type")["elevation_ft"]
+    .transform("mean")
+)
+
+airports_transform["difference_from_type_average"] = (
+    airports_transform["elevation_ft"]
+    - airports_transform["average_elevation_by_type"]
+)
+
+airports_transform["is_above_type_average"] = (
+    airports_transform["elevation_ft"]
+    > airports_transform["average_elevation_by_type"]
+)
+
+show_only_above = st.checkbox(
+    "Show only airports above type average elevation"
+)
+
+if show_only_above:
+    airports_transform = airports_transform[
+        airports_transform["is_above_type_average"] == True
+    ]
+
+st.dataframe(
+    airports_transform[
+        [
+            "ident",
+            "name",
+            "type",
+            "elevation_ft",
+            "average_elevation_by_type",
+            "difference_from_type_average",
+            "is_above_type_average"
+        ]
+    ].head(100)
+)
