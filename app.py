816c139d0160f17540e7ff21b97ad3400d14c05b
Soroosh Nazem <soroosh.nazem@gmail.com>
2026-06-08 17:29:23 +0200

    Add airport search functionality

--- app.py
+++ app.py
@@ -285,3 +285,35 @@ st.dataframe(top_frequency_airports)
 st.bar_chart(
     top_frequency_airports.set_index("ident")["number_of_frequencies"]
 )
+
+
+st.subheader("Search Airport")
+
+search_text = st.text_input("Search airport by name")
+
+if search_text:
+    search_result = airports[
+        airports["name"].str.contains(
+            search_text,
+            case=False,
+            na=False
+        )
+    ]
+
+    st.write(f"Results found: {len(search_result)}")
+
+    st.dataframe(
+        search_result[
+            [
+                "ident",
+                "name",
+                "type",
+                "iso_country",
+                "municipality",
+                "scheduled_service"
+            ]
+        ]
+    )
+
+else:
+    st.info("Type an airport name to search.")
