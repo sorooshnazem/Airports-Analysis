bc8de2b843852bbbf95d19d8cda1970ce2afec44
Soroosh Nazem <soroosh.nazem@gmail.com>
2026-06-23 04:53:54 +0200

    frequencies sent to a separate file

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from sections.top_countries import show_top_countries
 from sections.type_statistics import show_type_statistics
 from sections.runways import show_runways
 from sections.transform_analysis import show_transform_analysis
+from sections.frequencies import show_frequencies
 
 def load_csv(file_path):
     try:
@@ -183,53 +184,10 @@ elif page == "Runways":
 
 elif page == "Frequencies":
 
-    st.subheader("Airport Frequencies Analysis")
-
-    airport_frequencies = filtered_airports.merge(
-        frequencies,
-        left_on="id",
-        right_on="airport_ref",
-        how="inner"
+    show_frequencies(
+        filtered_airports,
+        frequencies
     )
-
-    st.dataframe(
-        airport_frequencies[
-            [
-                "ident",
-                "name",
-                "type_x",
-                "iso_country",
-                "airport_ident",
-                "type_y",
-                "description",
-                "frequency_mhz"
-            ]
-        ].head(100)
-    )
-
-    frequency_stats = (
-        airport_frequencies
-        .groupby(["ident", "name"], as_index=False)
-        .agg(
-            number_of_frequencies=("frequency_mhz", "size"),
-            min_frequency=("frequency_mhz", "min"),
-            max_frequency=("frequency_mhz", "max"),
-            number_of_frequency_types=("type_y", "nunique")
-        )
-        .sort_values("number_of_frequencies", ascending=False)
-    )
-
-    st.subheader("Top Airports by Number of Frequencies")
-
-    top_frequency_airports = frequency_stats.head(20)
-
-    st.dataframe(top_frequency_airports)
-
-    if len(top_frequency_airports) > 0:
-        st.bar_chart(
-            top_frequency_airports.set_index("ident")["number_of_frequencies"]
-        )
-
 
 # -----------------------------
 # SEARCH
